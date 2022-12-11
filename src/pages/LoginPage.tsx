2f84781b141ab9fc8ea9895bd6aa87d0e0310629
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-11 16:04:12 +0700

    Add logo and text to login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,34 @@
+import Logo from '../resources/images/logos/complete-logo.png';
+import styled from 'styled-components';
+import * as Colors from '../constants';
+import { StyledBackgroundBeforeLogin } from '../components/Background';
+
+const StyledLogo = styled.img`
+    width: 10%;
+    height: auto
+`
+
+const StyledHorizontalLine = styled.hr`
+    color: white;
+    width: 15%;
+    margin-bottom: 1.5em;
+`
+
+const StyledText = styled.text`
+    font-family: Metropolis-SemiBold;
+    font-size: 1.5em;
+    color: ${(props) => props.color? props.color : Colors.WHITE100}
+`
 const LoginPage = () => {
     return (
-        <div>
-            <text>login</text>
-        </div>
+        <StyledBackgroundBeforeLogin>
+            <StyledLogo src={Logo}/>
+            <StyledHorizontalLine/>
+            <text>
+                <StyledText>Log in to</StyledText> &nbsp;
+                <StyledText color={Colors.YELLOW100}>continue.</StyledText>
+            </text>
+        </StyledBackgroundBeforeLogin>
     )
 }
 
