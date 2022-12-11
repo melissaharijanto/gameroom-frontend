6dc1d00bdd2698ee3b6acd5a83fbf70db9a8a225
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-11 17:32:27 +0700

    Add flex layouts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,15 +7,15 @@ const DefaultLayout = styled.div`
 `
 
 const LeftAlignedLayout = styled.div`
-display: grid;
-justify-items: start;
-align-items: center;
+    display: grid;
+    justify-items: start;
+    align-items: center;
 `
 
 const RightAlignedLayout = styled.div`
-display: grid;
-justify-items: end;
-align-items: center;
+    display: grid;
+    justify-items: end;
+    align-items: center;
 `
 
 const HorizontalGrid = styled.div`
@@ -34,11 +34,34 @@ const FlexHorizontalLayout = styled.div`
     flex-direction: row;
 `
 
+const LeftAlignedFlex = styled.div<{direction: string}>`
+    display: flex;
+    justify-items: center;
+    align-items: flex-start;
+    flex-direction: ${(props) => props.direction? props.direction : "row"}
+`
+
+const RightAlignedFlex = styled.div`
+    display: flex;
+    justify-items: flex-end;
+    align-items: center;
+`
+
+const CenterAlignedFlex = styled.div<{direction: string}>`
+    display: flex;
+    flex-direction: ${(props) => props.direction? props.direction : "row"}
+    justify-items: center;
+    align-items: center;
+`
+
 export {
     DefaultLayout, 
     LeftAlignedLayout, 
     RightAlignedLayout, 
     HorizontalGrid, 
     FlexVerticalLayout, 
-    FlexHorizontalLayout
+    FlexHorizontalLayout,
+    LeftAlignedFlex,
+    RightAlignedFlex,
+    CenterAlignedFlex
 };
