b43732b880768393702eea5b91f921d0494882f0
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-11 22:03:45 +0700

    Design login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,6 +2,8 @@ import Logo from '../resources/images/logos/complete-logo.png';
 import styled from 'styled-components';
 import * as Colors from '../constants';
 import { StyledBackgroundBeforeLogin } from '../components/Background';
+import { LoginForm } from '../components/Forms';
+import { LogIn, SignUp } from '../components/Button';
 
 const StyledLogo = styled.img`
     width: 10%;
@@ -15,19 +17,46 @@ const StyledHorizontalLine = styled.hr`
 `
 
 const StyledText = styled.text`
-    font-family: Metropolis-SemiBold;
+    font-family: Metropolis-Bold;
     font-size: 1.5em;
-    color: ${(props) => props.color? props.color : Colors.WHITE100}
+    color: ${(props) => props.color? props.color : Colors.WHITE100};
 `
+
+const Heading = styled.text`
+    padding-bottom: 1em;
+`
+
+const BackToLandingPageText = styled.a`
+    color: ${Colors.WHITE100};
+    text-decoration-line: underline;
+    font-family: Metropolis-Medium;
+    font-size: 1em;
+    transition: 0.5s ease;
+
+    :hover {
+        color: ${Colors.BLUE100};
+    }
+`
+
 const LoginPage = () => {
     return (
         <StyledBackgroundBeforeLogin>
             <StyledLogo src={Logo}/>
             <StyledHorizontalLine/>
-            <text>
+            <Heading>
                 <StyledText>Log in to</StyledText> &nbsp;
                 <StyledText color={Colors.YELLOW100}>continue.</StyledText>
-            </text>
+            </Heading>
+            <LoginForm/>
+            <br/>
+            <LogIn marginRight="0em">Log In</LogIn>
+            <br/>
+            <StyledHorizontalLine/>
+            <StyledText>Don't have an account?</StyledText>
+            <br/>
+            <SignUp>Sign Up</SignUp>
+            <br/>
+            <BackToLandingPageText href="/">&#129040; Back</BackToLandingPageText>
         </StyledBackgroundBeforeLogin>
     )
 }
