8c40834cb75bd5cb6d9e174b45139de3c54fca09
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-16 18:13:11 +0700

    Add hook to dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,24 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const DashboardPage = () => {
     const location = useLocation();
 
     let user = location.state.user;
+
+    useEffect(() => {
+        axios.get('https://gameroom-api.onrender.com/authentication', {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => {
+                console.log(response);
+            }).catch((error) => {
+                console.log(error.response);
+            })
+    }, [])
 
     return (
         <div>
