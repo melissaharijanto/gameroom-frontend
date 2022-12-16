22e9a02d53b4bd64aec70b0a567e568893d1075a
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-16 17:00:36 +0700

    Add route to dashboard page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
+import DashboardPage from './pages/DashboardPage';
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" index element={<LandingPage />}/>
         <Route path="login" element={<LoginPage />}/>
         <Route path="signup" element={<SignUpPage />}/>
+        <Route path="dashboard" element={<DashboardPage/>}/>
       </Routes>
     </BrowserRouter>
   );
