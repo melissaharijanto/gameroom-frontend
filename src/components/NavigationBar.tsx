47ad8c88f00f6af661e991aa1ad8eb4f530635fd
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-30 22:59:23 +0700

    Add navigation bar skeleton

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,6 +1,59 @@
+import styled from 'styled-components';
+import * as Constants from '../constants';
+import Console from '../resources/images/logos/console-only.png';
+import WhiteTextLogo from '../resources/images/logos/white-text.png';
+import { SignUp } from './Button';
+
+const StyledNavBar = styled.div`
+    overflow: hidden;
+    background: linear-gradient(to right, ${Constants.MAGENTA100}, ${Constants.BLUE100});
+    width: 100%;
+`
+
+const StyledConsole = styled.img`
+    float: left;
+    display: block;
+    margin-top: auto;
+    margin-bottom: auto;
+    padding: 5px 5px 5px 20px;
+    width: auto;
+    height: 3vw;
+`
+const StyledWhiteText = styled.img`
+    float: left;
+    display: block;
+    margin-top: auto;
+    margin-bottom: auto;
+    padding: 10px 1px;
+    width: auto;
+    height: 2.5vw;
+`
+
+const StyledHomeIcon = styled.a`
+    float: right;
+    margin-top: auto;
+    margin-bottom: auto;
+    padding: 10px 1px;
+    width: auto;
+    height: 1.5vw;
+`
+
+const StyledSearchBar = styled.input`
+    display: block;
+    float: right;
+    margin-top: auto;
+    margin-bottom: auto;
+`
+
 const NavigationBar = () => {
     return (
-        <div></div>
+        <StyledNavBar>
+            <StyledConsole src={Console}/>
+            <StyledWhiteText src={WhiteTextLogo}/>
+            <StyledSearchBar type="text" placeholder="Search.."/>
+            <StyledHomeIcon>test</StyledHomeIcon>
+            <SignUp>Log Out</SignUp>
+        </StyledNavBar>
     )
 }
 
