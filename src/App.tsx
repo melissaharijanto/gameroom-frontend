ce845d6a8b9ee452f96e0753f0187a616f4d3bdf
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-04 16:45:56 +0700

    Redirect 404 to landing

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
         <Route path="login" element={<LoginPage />}/>
         <Route path="signup" element={<SignUpPage />}/>
         <Route path="dashboard" element={<DashboardPage />}/>
+        <Route path="*" element={<LandingPage/>}/>
       </Routes>
     </BrowserRouter>
   );
