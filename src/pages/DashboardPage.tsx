c34560f2d90fc1e0a51ea04f9d733a12aa874a63
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-04 23:15:59 +0700

    Change container layout to grid

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -5,12 +5,23 @@ import { BlackBackground } from "../components/Background";
 import NavigationBar from "../components/NavigationBar";
 import * as Constants from "../constants";
 import GameCommunityContainer from "../components/GameCommunityContainer";
-import { GameCommunity } from "../compiler/GameCommunity";
+import { GameCommunity } from "../compiler/interface/GameCommunity";
+import { useAuth } from "../compiler/context/Authentication";
 
 const StyledDiv = styled.div`
     padding-left: 2vw;
     padding-top: 2vw;
 `
+
+const StyledGrid = styled(StyledDiv)`
+    padding-right: 2vw;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-row-gap: 2vw;
+    justify-items: center;
+    align-items: center;
+`
+
 const StyledText = styled.text`
     color: ${(props) => props.color || Constants.WHITE100};
     font-family: Metropolis-ExtraBold;
@@ -18,23 +29,23 @@ const StyledText = styled.text`
 `
 
 const DashboardPage = () => {
-    const [user, setUser] = useState("");
+    const auth = useAuth();
 
     const [games, setGames] = useState<GameCommunity[]>([]);
 
-    const fetchCurrentUser = () => {
-        axios.get(Constants.API_ENDPOINT + '/current_user', {
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Bearer ${sessionStorage.getItem("gameroom")}`
+    /**
+     * Shuffles the array with the Fisher-Yates algorithm.
+     * From https://sebhastian.com/fisher-yates-shuffle-javascript/
+     * 
+     * @param arr The array to shuffle.
+     */
+    const fisherYatesShuffle = (arr: GameCommunity[]): GameCommunity[] => {
+        let i = arr.length;
+        while (--i > 0) {
+            let randIndex = Math.floor(Math.random() * (i + 1));
+                [arr[randIndex], arr[i]] = [arr[i], arr[randIndex]];
             }
-        }).then((response) => {
-                console.log(response);
-                setUser(response.data.username);
-            }).catch((error) => {
-                console.log(error.response);
-            })
+            return arr;
     }
 
     const fetchGames = () => {
@@ -45,17 +56,15 @@ const DashboardPage = () => {
                 'Authorization': `Bearer ${sessionStorage.getItem("gameroom")}`
             }
         }).then((response) => {
-            console.log(response.data);
-            console.log(response.data[0].image_url)
-            var arr = response.data.slice(0,9);
-            setGames(arr);
+            var arr = response.data as GameCommunity[];
+            var shuffled = fisherYatesShuffle(arr);
+            setGames(shuffled.splice(0, 9));
         }).catch((error) => {
             console.log(error.response);
         })
     }
 
     useEffect(() => {
-        fetchCurrentUser();
         fetchGames();
     }, []);
     
@@ -64,18 +73,18 @@ const DashboardPage = () => {
             <NavigationBar/>
             <StyledDiv>
                 <StyledText>Welcome, </StyledText>
-                <StyledText color={Constants.YELLOW100}>@{user}</StyledText>
+                <StyledText color={Constants.YELLOW100}>@{auth?.user.username}</StyledText>
                 <StyledText>.</StyledText>
             </StyledDiv>
-            <StyledDiv>
-                <StyledText>
+            <StyledGrid>
                 {games.map(game => {
-                    return <GameCommunityContainer
-                        title={game.title}
-                        image_url={game.image_url} />
+                    return (
+                        <div>
+                            <GameCommunityContainer game={game} />
+                        </div>
+                    )
                 })}
-                </StyledText>
-            </StyledDiv>
+            </StyledGrid>
         </BlackBackground>
     );
 }
