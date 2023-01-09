4470da025909ee6d7817d7c8821b0a640235db64
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2023-01-10 02:56:52 +0800

    Finalize design

--- src/pages/CommunityPage.tsx
+++ src/pages/CommunityPage.tsx
@@ -4,16 +4,36 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GameCommunity, GameCommunityInitialValue } from "../compiler/interface/GameCommunity";
 import { BlackBackground } from "../components/Background";
-import { API_ENDPOINT } from "../constants";
+import * as Constants from "../constants";
 import NavigationBar from "../components/NavigationBar";
 import CommunityHeader from "../components/CommunityHeader";
+import { CreatePost } from "../components/Button";
+
+const ForumWall = styled.span`
+    color: ${Constants.WHITE100};
+    font-family: Metropolis-ExtraBold;
+    font-size: 2em;
+`
+
+const Div = styled.div`
+    padding-left: 7vw;
+    padding-right: 7vw;
+`
+
+const HorizontalLine = styled.hr`
+    background-color: ${Constants.MAGENTA100};
+    border: none;
+    height: 2px;
+    margin: 0.5vw 0;
+    width: 15vw;
+`
 
 const CommunityPage = () => {
     let { id } = useParams();
     const [gameCommunity, setGameCommunity] = useState<GameCommunity>(GameCommunityInitialValue);
 
     const fetchGameCommunityData = () => {
-        axios.get(API_ENDPOINT + `/game_communities/${id}`)
+        axios.get(Constants.API_ENDPOINT + `/game_communities/${id}`)
             .then(response => setGameCommunity(response.data))
             .catch(error => console.log(error));
     }
@@ -25,6 +45,14 @@ const CommunityPage = () => {
         <BlackBackground>
             <NavigationBar/>
             <CommunityHeader game={gameCommunity}/>
+            <br/>
+            <br/>
+            <br/>
+            <Div>
+                <CreatePost>+ Create New Post</CreatePost>
+                <ForumWall>Forum Wall</ForumWall>
+                <HorizontalLine/>
+            </Div>
         </BlackBackground>
     )
 }
